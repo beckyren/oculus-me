d0f8ca48df138768df9f82e56503d78cff75c0f7
Becky <beckyren@udel.edu>
2021-05-31 14:47:01 -0400

    commit 1

---
+++ Assets/VideoManager.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Video;
+public class VideoManager : MonoBehaviour
+{
+    private VideoPlayer videoPlayer;
+    // Start is called before the first frame update
+    public void Start()
+    {
+        videoPlayer = GetComponent<VideoPlayer>();
+    }
+
+    public void Play()
+    {
+        
+        videoPlayer.Play();
+    }
+    public void Pause()
+    {
+        videoPlayer.Pause();
+    }
+
+    public void Stop()
+    {
+        videoPlayer.Stop();
+    }
+
+    public void UrlToVideo(string URL)
+    {
+        videoPlayer.source = VideoSource.Url;
+        videoPlayer.url = URL;
+        videoPlayer.Prepare();
+        videoPlayer.prepareCompleted += VideoPlayer_prepareCompleted;
+    }
+
+    private void VideoPlayer_prepareCompleted(VideoPlayer source)
+    {
+        Play();
+    }
+}
